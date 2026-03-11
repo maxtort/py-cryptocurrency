fb14623d0c08ad5034956099f86be7a73a27ce06
maxtort <topormaks3217@gmail.com>
2026-03-11 22:04:59 +0200

    Solution 2.0

--- app/test_main.py
+++ app/test_main.py
@@ -7,11 +7,21 @@ def test_when_prediction_above_5_percent() -> None:
         assert cryptocurrency_action(100) == "Buy more cryptocurrency"
 
 
-def test_when_prediction_below_5_percent() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction", return_value=94):
-        assert cryptocurrency_action(90) == "Do nothing"
-
-
 def test_when_prediction_is_not_that_much() -> None:
-    with mock.patch("app.main.get_exchange_rate_prediction", return_value=82):
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=94):
+        assert cryptocurrency_action(96) == "Do nothing"
+
+
+def test_when_prediction_below_5_percent() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=74):
         assert cryptocurrency_action(80) == "Sell all your cryptocurrency"
+
+
+def test_rate_105_percent_do_nothing() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=105):
+        assert cryptocurrency_action(100) == "Do nothing"
+
+
+def test_rate_95_percent_do_nothing() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=95):
+        assert cryptocurrency_action(100) == "Do nothing"
