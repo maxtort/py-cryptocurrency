74c9be764b583a33741905b16e949bc5b60bf0c6
maxtort <topormaks3217@gmail.com>
2026-03-11 21:39:02 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import cryptocurrency_action
+from unittest import mock
+
+
+def test_when_prediction_above_5_percent() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=106):
+        assert cryptocurrency_action(100) == "Buy more cryptocurrency"
+
+
+def test_when_prediction_below_5_percent() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=94):
+        assert cryptocurrency_action(90) == "Do nothing"
+
+
+def test_when_prediction_is_not_that_much() -> None:
+    with mock.patch("app.main.get_exchange_rate_prediction", return_value=82):
+        assert cryptocurrency_action(80) == "Sell all your cryptocurrency"
